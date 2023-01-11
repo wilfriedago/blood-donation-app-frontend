59e5b71ee74cb0274229e6760447eb1c2c3ad6f8
Wilfried AGO <wilfriedago@pm.me>
2023-01-12 00:53:40 +0100

    fix: basePath error in ci workflow

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://example.com', // FIXME: Change to the production URL
+  siteUrl: 'https://wilfriedago.github.io/blood-donation-app-frontend/',
   generateRobotsTxt: true,
 };
