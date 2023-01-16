3615fcc7214b59658cd99b764bd0546ed6ba9d29
Wilfried AGO <wilfriedago@pm.me>
2023-01-16 11:05:10 +0100

    feat: implementing email validation pages

--- src/pages/auth/confirm-email/[hash].tsx
+++ src/pages/auth/confirm-email/[hash].tsx
@@ -1,5 +1,9 @@
+import {
+  CheckCircleIcon,
+  ExclamationCircleIcon,
+} from '@heroicons/react/20/solid';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { InfoLayout, Meta } from '@/layouts';
 import api from '@/services/api';
@@ -7,16 +11,27 @@ import api from '@/services/api';
 export default function ConfirmEmail() {
   const router = useRouter();
   const { hash } = router.query;
+  const [status, setStatus] = useState<'loading' | 'error' | 'success'>(
+    'loading'
+  );
+
+  async function handleConfirmEmail() {
+    api
+      .confirmEmail(hash as string)
+      .then(() => {
+        setStatus('success');
+        setTimeout(() => {
+          router.push('/auth/login');
+        }, 8000);
+      })
+      .catch(() => {
+        setStatus('error');
+      });
+  }
 
   useEffect(() => {
-    if (hash)
-      api
-        .confirmEmail(hash as string)
-        .then(() => {})
-        .catch(() => {});
-
-    return () => {};
-  });
+    if (hash) handleConfirmEmail();
+  }, [hash]);
 
   return (
     <InfoLayout
@@ -26,18 +41,49 @@ export default function ConfirmEmail() {
           description="Vérifiez votre boîte de réception pour confirmer votre adresse email."
         />
       }
-      nextLink={{
-        label: 'Se connecter',
-        href: '/auth/login',
-      }}
     >
-      <div className="max-w-xl px-5 text-center">
-        <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Vérification email
-        </h1>
-        <p className="mb-2 text-lg text-zinc-500">
-          Vérifiez votre boîte de réception pour confirmer votre adresse email.
-        </p>
+      <div className="flex max-w-xl flex-col px-5 text-center">
+        {status === 'loading' && (
+          <>
+            <h1 className="mb-2 text-center text-3xl font-bold text-gray-900">
+              <div
+                className="mr-2 inline-block h-8 w-8 animate-spin rounded-full border-8 border-current border-t-transparent "
+                role="status"
+                aria-label="loading"
+              >
+                <span className="sr-only">Loading...</span>
+              </div>
+              Vérification email...
+            </h1>
+            <p className="text-center text-base">
+              Veuillez patienter pendant que nous vérifions votre email.
+            </p>
+          </>
+        )}
+        {status === 'error' && (
+          <>
+            <ExclamationCircleIcon className="h-14 text-indigo-500" />
+            <h1 className="mb-2 text-center text-3xl font-bold text-gray-900">
+              Erreur
+            </h1>
+            <p className="text-center text-base">
+              Une erreur s&apos;est produite lors de la vérification de votre
+              email.
+            </p>
+          </>
+        )}
+        {status === 'success' && (
+          <>
+            <CheckCircleIcon className="h-14 text-indigo-500" />
+            <h1 className="mb-2 text-center text-3xl font-bold text-gray-900">
+              Vérification email
+            </h1>
+            <p className="text-center text-base">
+              Votre email a été vérifié avec succès. Vous allez être redirigé
+              vers la page de connexion.
+            </p>
+          </>
+        )}
       </div>
     </InfoLayout>
   );
